8b85cdbd01cffecc461876391e9e17ea5e1ea293
shortnd <ococncol@gmail.com>
2020-12-12 21:25:16 -0500

    added more tests for auth

--- tests/Feature/LogoutTest.php
+++ tests/Feature/LogoutTest.php
@@ -25,7 +25,6 @@ class LogoutTest extends TestCase
 
     public function testLogoutReturnsSuccess()
     {
-        $this->withoutExceptionHandling();
         $this->actingAs(User::first())
             ->postJson('/logout')
             ->assertStatus(204);
--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class RegisterTest extends TestCase
@@ -23,5 +25,21 @@ class RegisterTest extends TestCase
             'password' => 'password',
             'password_confirmation' => 'password'
         ])->assertStatus(204);
+    }
+
+    public function testRegisterReturnsErrorWithSameEmail()
+    {
+        User::factory()->create([
+            'name' => 'test',
+            'email' => 'email@email.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $this->postJson('/register', [
+            'name' => 'test',
+            'email' => 'email@email.com',
+            'password' => 'password',
+            'password_confirmation' => 'password'
+        ])->assertStatus(422);
     }
 }
