2b74dd47c7463b641d4527e9421196944d03ed6a
shortnd <ococncol@gmail.com>
2020-12-12 20:08:53 -0500

    auth tests

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -12,6 +12,11 @@ use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->only(['logout']);
+    }
+
     public function register(Request $request): JsonResponse
     {
         $validated = Validator::make($request->all(), [
---
+++ tests/Feature/LogoutTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LogoutTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        User::factory()->create();
+    }
+
+    public function testLogoutReturnsUnauthenticated()
+    {
+        $this->postJson('/logout')->assertStatus(401);
+    }
+
+    public function testLogoutReturnsSuccess()
+    {
+        $this->withoutExceptionHandling();
+        $this->actingAs(User::first())
+            ->postJson('/logout')
+            ->assertStatus(204);
+    }
+}
